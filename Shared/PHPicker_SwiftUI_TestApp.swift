b7a093ea7cd93ebb8464e79939197ae75b1c3d02
Steven Marcotte <sdmarcotte@gmail.com>
2020-06-24 15:12:22 -0500

    Initial Commit

---
+++ Shared/PHPicker_SwiftUI_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PHPicker_SwiftUI_TestApp.swift
+//  Shared
+//
+//  Created by Blu Monkey on 6/24/20.
+//
+
+import SwiftUI
+
+@main
+struct PHPicker_SwiftUI_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
